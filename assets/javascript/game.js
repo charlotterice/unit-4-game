60f066764c1a195dbcbd97c6fb2e4a49ca58ae0a
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-03-17 00:03:33 -0400

    start of Javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,49 @@
-$(document).ready(function() {
+var computerChoice = Math.floor(Math.random()*120)+19
+var greenCrystal =Math.floor(Math.random()*12)+1;
+var yellowCrystal =Math.floor(Math.random()*12)+1;
+var blueCrystal =Math.floor(Math.random()*12)+1;
+var redCrystal =Math.floor(Math.random()*12)+1;
+var wins =0;
+var losses =0;
 
-    // Your code here...
+$(document).ready(function() {
     
+
     
     });
+
+
+    // function startup(){
+    //     wins=0
+    //     losses=0
+    //     userInput=[];
+    // }
+    
+    // document.onkeydown = function(event) {
+    //     var userGuess = event.key;
+    //    if (letterArray.indexOf(userGuess) > -1) {
+    
+    //        if (userGuess === computerChoice) {
+    //            wins++;
+    //            lives = 10;
+    //            userInput = [];
+    //        }
+           
+    //        if (userGuess != computerChoice) {
+    //            lives --;
+    //            userInput.push(userGuess);
+    //        }
+    
+    //        if (lives === 0) 
+    //        {
+    //        lives = 10;
+    //        losses ++;
+    //        userInput = [];
+    //        } 
+    //    guessedSpan.innerHTML=event.key;
+    //    winsSpan.textContent=wins;
+    //    lossesSpan.textContent=losses;
+    //    livesSpan.textContent=lives;
+    // }
+    // }
+    
